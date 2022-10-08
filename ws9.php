efe47374032a2b402870fa8b1b2b35231285ae37
fahsfahg <fahsfahg@gmail.com>
2022-10-08 22:02:44 +0700

    first commit

--- ws9.php
+++ ws9.php
@@ -1,6 +1,8 @@
 <?php
 $pdo = new PDO("mysql:host=localhost; dbname=blueshop; charset=utf8", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); ?>
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
 <html>
 
 <head>
@@ -9,12 +11,14 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); ?>
 
 <body>
     <?php
-    $stmt = $pdo->prepare("SELECT name, address, email FROM member");
+    $stmt = $pdo->prepare("SELECT * FROM member");
     $stmt->execute();
     while ($row = $stmt->fetch()) {
         echo "ชื่อสมาชิก : " . $row["name"] . "<br>";
         echo "ที่อยู่ : " . $row["address"] . "<br>";
-        echo "อีเมลล์ : " . $row["email"] . "<br>";
+        echo "อีเมล์ : " . $row["email"] . "<br>";
+        echo "หมายเลขโทรศัพท์ : " . $row["mobile"] . "<br>";
+        echo "<a href='editform.php?username=" . $row["username"] . "'>Edit</a>";
         echo "<hr>\n";
     }
     ?>
