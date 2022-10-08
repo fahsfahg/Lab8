9d963354a703fd4f5108b1ccb92f33883c22c4cf
fahsfahg <fahsfahg@gmail.com>
2022-10-08 22:11:22 +0700

    second commit

--- ws2.php
+++ ws2.php
@@ -1,8 +1,6 @@
 <?php
 $pdo = new PDO("mysql:host=localhost; dbname=blueshop; charset=utf8", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
-
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); ?>
 <html>
 
 <head>
@@ -11,14 +9,12 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 <body>
     <?php
-    $stmt = $pdo->prepare("SELECT * FROM member");
+    $stmt = $pdo->prepare("SELECT name, address, email FROM member");
     $stmt->execute();
     while ($row = $stmt->fetch()) {
         echo "ชื่อสมาชิก : " . $row["name"] . "<br>";
         echo "ที่อยู่ : " . $row["address"] . "<br>";
-        echo "อีเมล์ : " . $row["email"] . "<br>";
-        echo "หมายเลขโทรศัพท์ : " . $row["mobile"] . "<br>";
-        echo "<a href='editform.php?username=" . $row["username"] . "'>Edit</a>";
+        echo "อีเมลล์ : " . $row["email"] . "<br>";
         echo "<hr>\n";
     }
     ?>
